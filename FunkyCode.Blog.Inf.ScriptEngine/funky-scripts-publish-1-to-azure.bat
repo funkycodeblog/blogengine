e12740810fd2bf5341a14d906f5169f997ad68b7
Maciej Szczudlo <maciek.szczudlo@gmail.com>
2020-07-15 15:08:49 +0200

    [healthcheck] update batch

--- FunkyCode.Blog.Inf.ScriptEngine/funky-scripts-publish-1-to-azure.bat
+++ FunkyCode.Blog.Inf.ScriptEngine/funky-scripts-publish-1-to-azure.bat
@@ -1,4 +1,4 @@
-funky-scripts upload -h http://funkycode.azurewebsites.net/ -p ./Articles/foreach-vs-for -o
+funky-scripts upload -h http://funkycode.azurewebsites.net/ -p ./Articles/aspnetcore-health-checks-01 -o
 rem funky-scripts upload -p ./Articles/benchmark-dot-net -o
 
  
